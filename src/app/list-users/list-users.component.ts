c670f2005467a7406f4894e530f061e3f36aeb3e
IvanRobles19 <107086571+IvanRobles19@users.noreply.github.com>
2024-01-03 14:53:25 -0700

    listado de usuarios

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ApiProv } from '../providers/api.prov';
+import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrl: './list-users.component.css'
+  styleUrl: './list-users.component.css',
+  providers: [DatePipe],
 })
 export class ListUsersComponent {
+
+  public users : any;
+
+  constructor(private apiProv:ApiProv,private router: Router,private datePipe: DatePipe
+  ){
+      this.getCars();
+  }
+
+  public getCars() {
+    this.apiProv.getUsers().then((response) => {
+      this.users = response.data;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  formatDate(date: Date): string {
+    // Usa el DatePipe para formatear la fecha como "dd/MM/yyyy"
+    return this.datePipe.transform(date, 'dd/MM/yyyy') || '';
+  }
 
 }
